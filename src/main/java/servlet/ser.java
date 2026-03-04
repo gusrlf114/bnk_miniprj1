3fcc99513ba995f8fbfac682d5cf4e658b899dd6
khg <gusrlf116@naver.com>
2026-03-04 10:47:27 +0900

    new

---
+++ src/main/java/servlet/ser.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ser
+ */
+@WebServlet("/ser")
+public class ser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("123123123123213123");
+	}
+
+}
